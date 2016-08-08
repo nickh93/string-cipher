d40fec6a756b92571a820271ff47ad9866d8b117
Nick Hurtado and Sam Peppard <nick@>
2016-08-08 15:16:55 -0400

    build first 4 ciphers

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,39 @@
 
-function capFL(str) {
+function cipher1(str) {
   var firstLetter = str.charAt(0).toUpperCase();
-  var middle = str.substring(1, str.length - 1)
   var lastLetter = str.charAt(str.length - 1).toUpperCase();
-  return console.log(firstLetter + middle + lastLetter);
+  var output = firstLetter + lastLetter;
+  console.log(output);
+  return output;
 }
+
+var cipher2 = function(str) {
+  var firstLetter = str.charAt(0).toUpperCase();
+  var lastLetter = str.charAt(str.length - 1).toUpperCase();
+  var output = lastLetter + firstLetter;
+  console.log(output);
+  return output;
+}
+
 
 var sentence = prompt("enter a sentence");
 
-capFL(sentence);
+var cipher3 = function() {
+  var output = sentence + cipher2(cipher1(sentence));
+  console.log(output);
+  return output;
+}
+
+function cipher4(str) {
+  var indexHalf = str.length / 2;
+  var finalNumber = Math.floor(indexHalf);
+  var appendedLetter = str.charAt(finalNumber);
+  var output = appendedLetter + cipher3();
+  console.log(output);
+  return output;
+}
+
+cipher4(sentence);
+
+
+//capFL(sentence);
